38370de24f08e10591fcceaeb6a10fdf7a6bf6c0
Bislan <bislan_taymaskhanov@mail.ru>
2020-11-11 17:20:52 +0300

    Updates

---
+++ precache-manifest.2318a44f4d91d07219e0724529cbae7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de33c0936d6120ec529ca2c6fe63d898",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a262c0e141ec761eeb44",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "653cb2f03929fe48963b",
+    "url": "./static/css/main.fce361db.chunk.css"
+  },
+  {
+    "revision": "a262c0e141ec761eeb44",
+    "url": "./static/js/2.028a74d5.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.028a74d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653cb2f03929fe48963b",
+    "url": "./static/js/main.e4dc2a8d.chunk.js"
+  },
+  {
+    "revision": "5f526d65db2de3b6ee8f",
+    "url": "./static/js/runtime-main.0fd8efd7.js"
+  }
+]);
